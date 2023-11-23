b9cd057707ef29e90fd2de29ad4b7256c7767c33
cfilho <caugusto9871@gmail.com>
2023-11-22 20:57:51 -0400

    add surround and lightbar

--- lua/cfilho/packer.lua
+++ lua/cfilho/packer.lua
@@ -1,5 +1,33 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
+local packer_exists = pcall(vim.cmd, [[packadd packer.nvim]])
+
+if not packer_exists then
+	if vim.fn.input("Hent packer.nvim? (y for yada)") ~= "y" then
+		return
+	end
+
+	local directory = string.format(
+	'%s/site/pack/packer/opt/',
+	vim.fn.stdpath('data')
+	)
+
+	vim.fn.mkdir(directory, 'p')
+
+	local git_clone_cmd = vim.fn.system(string.format(
+	'git clone %s %s',
+	'https://github.com/wbthomason/packer.nvim',
+	directory .. '/packer.nvim'
+	))
+
+	print(git_clone_cmd)
+	print("Henter packer.nvim...")
+
+	return
+end
+
+
+
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
@@ -23,11 +51,18 @@ return require('packer').startup(function(use)
 	})
 
 	use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    
+    -- Surround
+	use 'tpope/vim-surround'
+
+    -- Repeat
+	use 'tpope/vim-repeat'
 
 	use ('nvim-treesitter/playground')
 	use ('theprimeagen/harpoon')
 	use ('mbbill/undotree')
 	use ('tpope/vim-fugitive')
+	use ('itchyny/lightline.vim')
 	use {
 		'VonHeikemen/lsp-zero.nvim',
 		branch = 'v2.x',
@@ -42,5 +77,14 @@ return require('packer').startup(function(use)
 			{'hrsh7th/cmp-nvim-lsp'}, -- Required
 			{'L3MON4D3/LuaSnip'},     -- Required
 		}
-	}
+    }
+    use({
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    })
 end)
