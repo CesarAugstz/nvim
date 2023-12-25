1322ce1b377ccf6b50859a796afd127aaff39eee
CFilho <caugusto9871@gmail.com>
2023-12-24 20:32:08 -0400

    new plugins

--- lua/cfilho/packer.lua
+++ lua/cfilho/packer.lua
@@ -1,24 +1,37 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
-	
-	
+
+
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.2',
 		-- or                            , branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 
-	use({ 
-		'rose-pine/neovim',
-		as = 'rose-pine',
+	use({
+		'catppuccin/nvim',
+		as = 'catppuccin',
 		config = function()
-			vim.cmd('colorscheme rose-pine')
+			vim.cmd('colorscheme catppuccin-mocha')
 		end
 	})
 
@@ -43,4 +56,32 @@ return require('packer').startup(function(use)
 			{'L3MON4D3/LuaSnip'},     -- Required
 		}
 	}
+
+    use ({
+        'feline-nvim/feline.nvim',
+        config = function()
+            require('feline').setup()
+        end
+    })
+
+    use ({
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    })
+
+    use ({
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require('gitsigns').setup()
+        end
+    })
+
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
