c809396a667b5500bbc9ef86237a05bdeb162771
CesarF <dev.caugustoaf@gmail.com>
2024-07-31 21:55:01 -0400

    add cody

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -1,55 +1,42 @@
 local line_ok, formatter = pcall(require, "formatter")
 
-if not line_ok then
-	return
-end
+if not line_ok then return end
 
 local prettier_filetypes = {
-	"javascript",
-	"typescript",
-	"vue",
-	"Vue",
-	"json",
-	"html",
-	"css",
-	"scss",
+  "javascript", "typescript", "vue", "Vue", "json", "html", "css", "scss"
 }
 
 local function is_prettier_filetype(filetype)
-	for i = 1, #prettier_filetypes do
-		if prettier_filetypes[i] == filetype then
-			return true
-		end
-	end
-	return false
+  for i = 1, #prettier_filetypes do
+    if (prettier_filetypes[i] == filetype) then return true end
+  end
+  return false
 end
 
 formatter.setup({
-	logging = true,
-	log_level = vim.log.levels.DEBUG,
-	filetype = {
-		lua = {
-			-- luafmt
-			function()
-				return {
-					exe = "stylua",
-					args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0) },
-				}
-			end,
-		},
-		["*"] = {
-			-- default vim
-			function()
-				if is_prettier_filetype(vim.bo.filetype) then
-					return {
-						exe = "prettierd",
-						args = { vim.api.nvim_buf_get_name(0) },
-						stdin = true,
-					}
-				end
-				vim.lsp.buf.format()
-				return nil
-			end,
-		},
-	},
+  logging = true,
+  log_level = vim.log.levels.DEBUG,
+  ignore_exitcode = false,
+  filetype = {
+    lua = {
+      -- luafmt
+      function()
+        return {exe = "lua-format", args = {"--indent-width", 2}, stdin = true}
+      end
+    },
+    ["*"] = {
+      -- default vim
+      function()
+        if is_prettier_filetype(vim.bo.filetype) then
+          return {
+            exe = "prettierd",
+            args = {vim.api.nvim_buf_get_name(0)},
+            stdin = true
+          }
+        end
+        vim.lsp.buf.format()
+        return nil
+      end
+    }
+  }
 })
